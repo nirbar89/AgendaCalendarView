df9211a5978113852b139c11b02e680389bf6fc6
nirbar89 <60570177+nirbar89@users.noreply.github.com>
2020-03-11 23:40:31 +0200

    Update EventAdapter.java

--- agendacalendar/src/main/java/com/jaiselrahman/agendacalendar/view/EventAdapter.java
+++ agendacalendar/src/main/java/com/jaiselrahman/agendacalendar/view/EventAdapter.java
@@ -3,6 +3,7 @@ package com.jaiselrahman.agendacalendar.view;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
