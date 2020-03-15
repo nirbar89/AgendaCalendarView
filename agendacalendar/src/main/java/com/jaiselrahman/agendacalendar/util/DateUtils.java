cb583cf80e7ed0f1bf6b238168b209fe7382ece1
nirbar89 <60570177+nirbar89@users.noreply.github.com>
2020-03-15 10:02:23 +0200

    Update DateUtils.java

--- agendacalendar/src/main/java/com/jaiselrahman/agendacalendar/util/DateUtils.java
+++ agendacalendar/src/main/java/com/jaiselrahman/agendacalendar/util/DateUtils.java
@@ -3,6 +3,9 @@ package com.jaiselrahman.agendacalendar.util;
 import org.threeten.bp.DayOfWeek;
 import org.threeten.bp.LocalDate;
 import org.threeten.bp.LocalDateTime;
+import org.threeten.bp.ZoneId;
+import org.threeten.bp.format.DateTimeFormatter;
+import org.threeten.bp.temporal.TemporalAdjusters;
 import org.threeten.bp.temporal.WeekFields;
 
 import java.util.Locale;
@@ -15,21 +18,25 @@ public class DateUtils {
                 && today.getDayOfYear() == localDateTime.getDayOfYear();
     }
 
-    public static String[] getDaysOfWeek() {
-        DayOfWeek firstDayOfWeek = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
+//    public static String[] getDaysOfWeek() {
+//        DayOfWeek firstDayOfWeek = WeekFields.of(Locale.getDefault()).getFirstDayOfWeek();
+//
+//        DayOfWeek[] daysOfWeek = DayOfWeek.values();
+//        String[] daysOfWeek2 = new String[daysOfWeek.length];
+//
+//        if (firstDayOfWeek != DayOfWeek.MONDAY) {
+//            int j = 0;
+//            for (int i = firstDayOfWeek.ordinal(); i < daysOfWeek.length; i++, j++)
+//                daysOfWeek2[j] = String.valueOf(daysOfWeek[i].toString().charAt(0));
+//
+//            for (int i = 0; i < firstDayOfWeek.ordinal(); i++, j++)
+//                daysOfWeek2[j] = String.valueOf(daysOfWeek[i].toString().charAt(0));
+//        }
+//
+//        return daysOfWeek2;
+//    }
 
-        DayOfWeek[] daysOfWeek = DayOfWeek.values();
-        String[] daysOfWeek2 = new String[daysOfWeek.length];
-
-        if (firstDayOfWeek != DayOfWeek.MONDAY) {
-            int j = 0;
-            for (int i = firstDayOfWeek.ordinal(); i < daysOfWeek.length; i++, j++)
-                daysOfWeek2[j] = String.valueOf(daysOfWeek[i].toString().charAt(0));
-
-            for (int i = 0; i < firstDayOfWeek.ordinal(); i++, j++)
-                daysOfWeek2[j] = String.valueOf(daysOfWeek[i].toString().charAt(0));
-        }
-
-        return daysOfWeek2;
-    }
+public static String[] getDaysOfWeek(){ //TODO: ask Or about this
+    return new String[]{"א", "ב", "ג", "ד", "ה", "ו", "ש" };
+}
 }
